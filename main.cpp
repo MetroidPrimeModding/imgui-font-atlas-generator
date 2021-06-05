2bccc38871f8e7ee467b7dfc5121158bc7c4901f
Pwootage <pwootage@gmail.com>
2021-06-04 18:33:55 -0600

    Pack the font data smaller

--- main.cpp
+++ main.cpp
@@ -13,9 +13,11 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+
 #ifdef __APPLE__
 #define GL_SILENCE_DEPRECATION
 #endif
+
 #include <GLFW/glfw3.h>
 
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
@@ -25,20 +27,21 @@
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
-static void glfw_error_callback(int error, const char* description)
-{
+static void glfw_error_callback(int error, const char *description) {
   fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
+int float_to_char(int width, float f) {
+  return (unsigned char) (f * (float) width);
+}
 
 
-int main(int, char**)
-{
+int main(int, char **) {
   // Setup window
   glfwSetErrorCallback(glfw_error_callback);
   if (!glfwInit())
     return 1;
-  GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL2 example", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL2 example", NULL, NULL);
   if (window == NULL)
     return 1;
   glfwMakeContextCurrent(window);
@@ -50,7 +53,8 @@ int main(int, char**)
   atlas->TexDesiredWidth = 128;
   atlas->Flags |= ImFontAtlasFlags_NoMouseCursors;
   ImGui::CreateContext(atlas);
-  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  ImGuiIO &io = ImGui::GetIO();
+  (void) io;
   //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
   // Setup Dear ImGui style
@@ -75,19 +79,19 @@ int main(int, char**)
   fontConfig.OversampleH = 1;
   fontConfig.OversampleV = 1;
   static const ImWchar ranges[] =
-  {
-      0x0020, 0x007E, // Basic Latin + Latin Supplement
-      0,
-  };
+      {
+          0x0020, 0x007E, // Basic Latin + Latin Supplement
+          0,
+      };
   fontConfig.GlyphRanges = &ranges[0];
   io.Fonts->AddFontFromFileTTF("imgui/misc/fonts/ProggyTiny.ttf", 10.0f, &fontConfig);
 
   // alright we made our font. Dump that shit.
-  unsigned char* texData;
+  unsigned char *texData;
   int width, height;
   io.Fonts->GetTexDataAsAlpha8(&texData, &width, &height);
 
-  unsigned char* outData = new unsigned char[width * height / 2];
+  unsigned char *outData = new unsigned char[width * height / 2];
   memset(outData, 0, width * height / 2);
 
   constexpr int blockWidth = 8;
@@ -130,6 +134,29 @@ int main(int, char**)
   out << "#pragma once\n\n";
   out << "#include \"imgui.h\"\n\n";
   out << "namespace FontAtlas {\n";
+
+  out << "struct FontChar {\n";
+  out << "bool Colored: 1;\n";
+  out << "bool Visible: 1;\n";
+  out << "unsigned char Codepoint;\n";
+  out << "char AdvanceX;\n";
+  out << "char X0;\n";
+  out << "char Y0;\n";
+  out << "char X1;\n";
+  out << "char Y1;\n";
+  out << "unsigned char U0;\n";
+  out << "unsigned char V0;\n";
+  out << "unsigned char U1;\n";
+  out << "unsigned char V1;\n";
+  out << "};\n\n";
+
+  out << "struct CharVec4 {\n";
+  out << "unsigned char W;\n";
+  out << "unsigned char X;\n";
+  out << "unsigned char Y;\n";
+  out << "unsigned char Z;\n";
+  out << "};\n\n";
+
   // font info
   ImFont *font = io.Fonts->Fonts[0];
   const ImFontConfig *configData = font->ConfigData;
@@ -140,7 +167,7 @@ int main(int, char**)
     out << v << ",";
   }
   out << "\n};\n";
-  out << "constexpr float FALLBACK_ADVANCE_X = " << font->FallbackAdvanceX  << ";\n";
+  out << "constexpr float FALLBACK_ADVANCE_X = " << font->FallbackAdvanceX << ";\n";
   out << "constexpr int FONT_SIZE = " << font->FontSize << ";\n";
 
   out << "const ImWchar INDEX_LOOKUP[] = {\n";
@@ -149,18 +176,18 @@ int main(int, char**)
     out << v << ",";
   }
   out << "\n};\n";
-  out << "const ImFontGlyph FONT_GLYPHS[] = {\n";
+  out << "const FontChar FONT_GLYPHS[] = {\n";
   for (int i = 0; i < font->Glyphs.Size; i++) {
     ImFontGlyph *v = &font->Glyphs[i];
-    out  << "{";
+    out << "{";
     out << ".Colored=" << (v->Colored ? "true" : "false");
     out << ",.Visible=" << (v->Visible ? "true" : "false");
     out << ",.Codepoint=" << v->Codepoint;
     out << ",.AdvanceX=" << v->AdvanceX;
     out << ",.X0=" << v->X0 << ",.Y0=" << v->Y0;
     out << ",.X1=" << v->X1 << ",.Y1=" << v->Y1;
-    out << ",.U0=" << v->U0 << ",.V0=" << v->V0;
-    out << ",.U1=" << v->U1 << ",.V1=" << v->V1;
+    out << ",.U0=" << float_to_char(atlas->TexWidth, v->U0) << ",.V0=" << float_to_char(atlas->TexHeight, v->V0);
+    out << ",.U1=" << float_to_char(atlas->TexWidth, v->U1) << ",.V1=" << float_to_char(atlas->TexHeight, v->V1);
     out << "},\n";
   }
   out << "};\n";
@@ -169,11 +196,11 @@ int main(int, char**)
   out << "constexpr float DESCENT = " << font->Descent << ";\n";
   out << "constexpr float MetricsTotalSurface = " << font->MetricsTotalSurface << ";\n";
 
-  out << "const ImVec2 WhitePixel{"<< atlas->TexUvWhitePixel.x << "," << atlas->TexUvWhitePixel.y << "};\n";
+  out << "const ImVec2 WhitePixel{" << atlas->TexUvWhitePixel.x << "," << atlas->TexUvWhitePixel.y << "};\n";
 
   out << "constexpr unsigned char Used4kPagesMap[(0xFFFF+1)/4096/8] = {\n";
   for (auto &v : font->Used4kPagesMap) {
-    out << (int)v << ",";
+    out << (int) v << ",";
   }
   out << "\n};\n";
 
@@ -184,9 +211,13 @@ int main(int, char**)
   out << "const int PackedIdLines = " << atlas->PackIdLines << ";\n";
   out << "const int PackIdMouseCursors = " << atlas->PackIdMouseCursors << ";\n";
 
-  out << "const ImVec4 TexUvLines[IM_DRAWLIST_TEX_LINES_WIDTH_MAX + 1] = {\n";
+  out << "const CharVec4 TexUvLines[IM_DRAWLIST_TEX_LINES_WIDTH_MAX + 1] = {\n";
   for (auto &v : atlas->TexUvLines) {
-    out << "ImVec4{" << v.w << ","<<v.x<<","<<v.y<<","<<v.z<<"},\n";
+    out << "CharVec4{"
+        << float_to_char(atlas->TexWidth, v.w) << ","
+        << float_to_char(atlas->TexHeight, v.x) << ","
+        << float_to_char(atlas->TexWidth, v.y) << ","
+        << float_to_char(atlas->TexHeight, v.z) << "},\n";
   }
   out << "\n};\n";
 
@@ -196,9 +227,9 @@ int main(int, char**)
 //    out << ".Width="<<v.Width;
 //    out << ",.Height="<<v.Height;
     out << v.Width;
-    out << ","<<v.Height;
-    out << ","<<v.X;
-    out << ","<<v.Y;
+    out << "," << v.Height;
+    out << "," << v.X;
+    out << "," << v.Y;
 //    out << ",.GlyphID="<<v.GlyphID;
 //    out << ",.GlyphAdvanceX=" << v.GlyphAdvanceX;
 //    out << ",.GlyphOffset={" << v.GlyphOffset.x << ","<< v.GlyphOffset.y << "}";
@@ -222,7 +253,7 @@ int main(int, char**)
       out << "\n\t";
       currentLine = 0;
     }
-    out << "0x" << (int)outData[i] << ",";
+    out << "0x" << (int) outData[i] << ",";
   }
   out << std::dec;
   out << "};\n\n";
@@ -231,14 +262,13 @@ int main(int, char**)
   out.close();
 
 
-      // Our state
+  // Our state
   bool show_demo_window = true;
   bool show_another_window = false;
   ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
   // Main loop
-  while (!glfwWindowShouldClose(window))
-  {
+  while (!glfwWindowShouldClose(window)) {
     // Poll and handle events (inputs, window resize, etc.)
     // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
     // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
@@ -260,28 +290,31 @@ int main(int, char**)
       static float f = 0.0f;
       static int counter = 0;
 
-      ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+      ImGui::Begin(
+          "Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
       ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
       ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
       ImGui::Checkbox("Another Window", &show_another_window);
 
       ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+      ImGui::ColorEdit3("clear color", (float *) &clear_color); // Edit 3 floats representing a color
 
-      if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+      if (ImGui::Button(
+          "Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
         counter++;
       ImGui::SameLine();
       ImGui::Text("counter = %d", counter);
 
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+                  ImGui::GetIO().Framerate);
       ImGui::End();
     }
 
     // 3. Show another simple window.
-    if (show_another_window)
-    {
-      ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+    if (show_another_window) {
+      ImGui::Begin("Another Window",
+                   &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
       ImGui::Text("Hello from another window!");
       if (ImGui::Button("Close Me"))
         show_another_window = false;
@@ -293,7 +326,8 @@ int main(int, char**)
     int display_w, display_h;
     glfwGetFramebufferSize(window, &display_w, &display_h);
     glViewport(0, 0, display_w, display_h);
-    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w,
+                 clear_color.w);
     glClear(GL_COLOR_BUFFER_BIT);
 
     // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!),
